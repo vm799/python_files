a3119eb2b8588d2ca45e55f7d41057e2717ee57a
Vaishali <vaishalimehmi@yahoo.co.uk>
2023-01-30 15:44:47 +0000

    updated movie function

--- Python38/watch_next.py
+++ Python38/watch_next.py
@@ -1,3 +1,4 @@
+
 import spacy 
 
 nlp = spacy.load("en_core_web_md")
@@ -7,65 +8,66 @@ nlp = spacy.load("en_core_web_md")
 
 # list of text to run similarity search on
 last_watched_movie = [
-    "Will he save their world or destroy it? When the hulk becomes too dangerous for the Earth, the illuninati trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace.  Unfortunately, Hulk lands on the planet Sakaar where he is sold to slavery and trained as a gladiator."
+    "Planet Hulk: Will he save their world or destroy it? When the hulk becomes too dangerous for the Earth, the illuninati trick Hulk into a shuttle and launch him into space to a planet where the Hulk can live in peace.  Unfortunately, Hulk lands on the planet Sakaar where he is sold to slavery and trained as a gladiator."
 ]
-
-# print("-------------Last watched movie similarity---------------")
-for token in last_watched_movie:
-    token = nlp(token)
-    for token_ in last_watched_movie:
-        token_ = nlp(token_)
-        # print(token.similarity(token_))
 
 #open file in read mode and return a list of movies to compare with last_watched_movie
 with open("Python38/movies.txt", "r") as movie_option_file:
     movie_options = movie_option_file.readlines()
-   
-#create a list of movies to compare with last_watched_movie
-for i in movie_options:
-    # print(i.split("\n"))
-    
-    # print("-------------Movies similarity---------------")
-    for token in movie_options:
-        token = nlp(token)
-        for token_ in movie_options:
-            token_ = nlp(token_)
-            # print(token.similarity(token_))
         
 # Get the extent of similarity between the last_watched_movie and movie_options.
 # loop through every movie in movie_options and compare it with last_watched.      
 
-# print("-------------Movies & last_watched similarity---------------")
+
+#("-------------Movies & last_watched similarity---------------")
 closely_matched_movies =[]
-for token in movie_options:
-    token = nlp(token)
-    for token_ in last_watched_movie:
-        token_ = nlp(token_)
-        if token.similarity(token_) > 0.80:
-            print(token.similarity(token_))
-            closely_matched_movies.append(token.similarity(token_))
-print(closely_matched_movies) 
-print(max(closely_matched_movies))  
 
-for i in closely_matched_movies:
-    if i == max(closely_matched_movies):
-        print(f"""
+def next_movie_recommendation(movies, last_movie):
+    
+    #iterating through each movie in the movie options
+    for token in movies:
+        token = nlp(token)
+        
+        #and through the last_movie string
+        for token_ in last_movie:
+            token_ = nlp(token_)
+            
+            #add all similarity indexes to a list
+            best_matched_movies = (token.similarity(token_), token)
+            closely_matched_movies.append(best_matched_movies)
 
-#------------------------------------------------------------#
+    #find the film with highest similarity index and print
+    best_next_movie = max(closely_matched_movies)
+    
+    #remove the index from the tuple
+    movie = best_next_movie[1]
+
+    #slice the tuple to print the title and contents
+    print(f"""
+
+#-----------------------------------------------------------------#
+    
 Here is your most similar movie matched from your list of movies.
          
-Watch next: 
+--**--Watch next--**--
         
-        {token}
-#------------------------------------------------------------#
+        
+**   {movie[0:2]}   **
+
+
+--**--What is it about?--**--
+
+{movie[3:]}
+        
+#------------------------------------------------------------------#
 
         """)
+    
+
+next_movie_recommendation(movie_options, last_watched_movie)          
             
             
-            
-           
-        
-        
+
 #https://www.tutorialspoint.com/python/list_max.htm
 #https://realpython.com/python-append/#:~:text=Python%20provides%20a%20method%20called,list%20using%20a%20for%20loop.
 # https://www.w3schools.com/python/ref_string_join.asp
